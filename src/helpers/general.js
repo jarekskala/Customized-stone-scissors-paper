cabffab6aaa7fde62d344ec50df671fa89d2eda6
Jaroslav Skála <jaroslav.skala2@gmail.com>
2023-03-22 18:49:59 +0100

    Delete src directory

--- src/helpers/general.js
+++
@@ -1,6 +0,0 @@
-export const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-export const findUserIndex = (player, store) => store.usersState.findIndex((current) => current.id === player);
-export const findUser = (player, store) => store.usersState.find((current) => current.id === player);
-
-
-
--- src/services/users.js
+++
@@ -1,7 +0,0 @@
-import { api, baseUrl } from './api';
-
-export function getUsers() {
-    return api.get(`${baseUrl}/users`);
-}
-
-
